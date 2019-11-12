a32f8202205b258245d71dd7ea07313f81e0e859
DAMIAN ORAMAS <damianoramas@DAMIANs-MBP.hsd1.or.comcast.net>
2019-11-12 14:08:38 -0800

    setting up custom user model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user.apps.UsersConfig',
+    'djngo.contrib.gis'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'user.User'
